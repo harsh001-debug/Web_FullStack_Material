460f09fa4c75e90cfedc8d6ef046918d346cb49f
harsh001-debug <harshkt2004@gmail.com>
2026-03-12 19:34:21 +0530

    Import React from Node Modules

---
+++ React/day2/apps.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+// document.addEventListener("click", function() {
+//     // alert("Button clicked!");
+//     const button = document.createElement('h1');
+//     button.innerHTML = "New Button Clicked";
+//     document.body.appendChild(button);
+// }
+
+const button = document.querySelector(".button");
+button.addEventListener('click',() =>{
+    
+})
