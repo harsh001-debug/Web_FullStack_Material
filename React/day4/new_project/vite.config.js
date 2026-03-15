e133ab73da7236562e060ed55d8b5bda55a11f46
harsh001-debug <harshkt2004@gmail.com>
2026-03-15 21:37:22 +0530

    Add remaining files

---
+++ React/day4/new_project/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// React\day5\.parcel-cache
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
